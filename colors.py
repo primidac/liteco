1b3f9a418b4116489c63a8e5198ed3c4a3f97081
primidac <primidac@gmail.com>
2019-12-01 20:08:14 +0100

    initial color file

---
+++ colors.py
@@ -0,0 +1,23 @@
+class Color:
+    def __init__(self):
+        
+        grey = '\033[1;30m\0'
+        red = '\033[1;31m\0'
+        green = '\033[1;32m\0'
+        yellow = '\033[1;33m\0'
+        blue = '\033[1;34m\0'
+        purple = '\033[1;35m\0'
+        cyan = '\033[1;36m\0'
+        white = '\033[1;37m\0'
+         
+        self.grey = grey
+        self.red = red
+        self.green = green
+        self.yellow = yellow
+        self.blue = blue
+        self.cyan = cyan
+        self.purple = purple
+        self.white = white
+
+show = Color() 
+
